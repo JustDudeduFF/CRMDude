77ec4ce180a04aa7e8996cf0418db5d700c70c78
unknown <authebaba.yt@gmail.com>
2024-09-01 02:21:04 +0530

    01-09-2024

--- src/components/Master/ISP.js
+++ src/components/Master/ISP.js
@@ -24,7 +24,7 @@ export default function ISP() {
                         
                     });
                     setArrayIsp(IspArray);
-                    console.log(IspArray);
+                    
                 }
             
         });

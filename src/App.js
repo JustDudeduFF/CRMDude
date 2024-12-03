49cc9eb27789b3fc3388210a13ea9aeb5b12d44a
JustDudeduFF <authebaba.yt@gmail.com>
2024-12-04 00:00:35 +0530

    03-12-2024

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes,Route  } from 'react-router-dom';
 import Login from './components/Login'
 import New_Dashboard from './components/New_Dashboard';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" Component={Login}/>
         <Route path='/dashboard/*' Component={New_Dashboard}/>
+        <SpeedInsights />
       </Routes> 
     </Router>
   );
